0d73d4629e70c3998007fcef7a203aa4a12687b7
Arteom Katkov <arteomk0626@gmail.com>
2023-10-25 16:00:16 -0500

    Base Buildroot Build works

---
+++ board/nuvoton/nuc9x0/install_nuc980_iot.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cp -a board/nuvoton/nuc9x0/quectel-CM output/target/usr/bin
---
+++ setup_linux.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd buildroot
+echo Setting up Base Buildroot Environment...
+make defconfig BR2_DEFCONFIG=../nuc_configs/nuvoton_nuc980_iot_defconfig
+
+
