a43d52e8a19ceca8d7c1568af6b723a0ff1a0e5e
Arteom Katkov <arteomk0626@gmail.com>
2023-11-04 20:35:23 +0000

    Add Rootfs overlay

--- board/nuvoton/nuc9x0/install_nuc980_iot.sh
+++ board/nuvoton/nuc9x0/install_nuc980_iot.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
 cp -a ../board/nuvoton/nuc9x0/quectel-CM output/target/usr/bin
+
+# Copy rootfs.tar and umage to mounted directory
+echo 'Copying rootfs.tar and uImage to mounted directory...'
+rm ../../../../buildroot-docker/MountedBuildroot/rootfs.tar
+rm ../../../../buildroot-docker/MountedBuildroot/uImage
+cp /Buildroot-NUC980/buildroot/output/images/rootfs.tar /buildroot-docker/MountedBuildroot/
+cp /Buildroot-NUC980/buildroot/output/images/uImage /buildroot-docker/MountedBuildroot/
+
+# TODO: Run install script to flash image to sdcard
